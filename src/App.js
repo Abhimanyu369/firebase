8f14ce15266bafc047eb41daebe0714b88983beb
Abhimanyu Malik <abhimanyumalik369@gmail.com>
2021-11-22 19:11:25 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import app from "./firebase";
+import { getMessaging, getToken } from "firebase/messaging";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  React.useEffect(() => {
+    // Get registration token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    const messaging = getMessaging();
+    getToken(messaging, {
+      vapidKey:
+        "BCQ0vD3yigVxsWqtLXfMH9VP2J4DK4ejUZtooR4wkIYj_7is_QLkvaWKJXua0brEu6wBsNInnwHvi8DSOWNUWVw",
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          // Send the token to your server and update the UI if necessary
+          console.log(currentToken);
+        } else {
+          // Show permission request UI
+          console.log(
+            "No registration token available. Request permission to generate one."
+          );
+          // ...
+        }
+      })
+      .catch((err) => {
+        console.log("An error occurred while retrieving token. ", err);
+        // ...
+      });
+  });
+  return <div className="">Hi My Push Notification service</div>;
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBd1lW5PxDi1dX9uSYKi5ROXZgavGrVcWA",
+  authDomain: "push-notification-94cb2.firebaseapp.com",
+  projectId: "push-notification-94cb2",
+  storageBucket: "push-notification-94cb2.appspot.com",
+  messagingSenderId: "322008088259",
+  appId: "1:322008088259:web:52a349d37fcdc27e34ec76",
+  measurementId: "G-97109E4K1Y",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
